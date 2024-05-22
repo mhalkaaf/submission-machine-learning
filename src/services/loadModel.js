71900698780f5a07900cabec5541d0ceb7f2e9ed
haeckal <haeckal@asaren.ai>
2024-05-22 17:37:04 +0700

    fix dotenv

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,5 +1,10 @@
 const tf = require('@tensorflow/tfjs-node');
 
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
 async function loadModel() {
     return tf.loadGraphModel(process.env.MODEL_URL);
 }
