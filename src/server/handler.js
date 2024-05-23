c526a880a95a2e5fc113084c115f588831610664
haeckal <haeckal@asaren.ai>
2024-05-23 08:31:29 +0700

    tes

--- src/server/handler.js
+++ src/server/handler.js
@@ -1,6 +1,7 @@
 const predictClassification = require('../services/inferenceService');
 const crypto = require('crypto');
 const storeData = require('../services/storeData');
+const InputError = require('../exceptions/InputError');
 const PredictionError = require('../exceptions/PredictionError');
 
 async function postPredictHandler(request, h) {
