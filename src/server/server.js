d658f1689c5ed332c11687a0b315874c8e55bbe2
haeckal <haeckal@asaren.ai>
2024-05-23 08:27:08 +0700

    tes

--- src/server/server.js
+++ src/server/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 const loadModel = require('../services/loadModel');
-// const InputError = require('../exceptions/InputError');
+const InputError = require('../exceptions/InputError');
 // const PredictionError = require('../exceptions/PredictionError');
  
 (async () => {
@@ -25,14 +25,14 @@ const loadModel = require('../services/loadModel');
     server.ext('onPreResponse', function (request, h) {
         const response = request.response;
 
-        // if (response instanceof InputError) {
-        //     const newResponse = h.response({
-        //         status: 'fail',
-        //         message: `${response.message} Silakan gunakan foto lain.`
-        //     });
-        //     newResponse.code(response.statusCode);
-        //     return newResponse;
-        // }
+        if (response instanceof InputError) {
+            const newResponse = h.response({
+                status: 'fail',
+                message: `${response.message} Silakan gunakan foto lain.`
+            });
+            newResponse.code(response.statusCode);
+            return newResponse;
+        }
 
         // if (response instanceof PredictionError) {
         //     const newResponse = h.response({
