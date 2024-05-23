3f1d6ed7cdcc08e945c3381c8e8d212787d0c06f
haeckal <haeckal@asaren.ai>
2024-05-23 07:45:11 +0700

    tes

--- src/server/server.js
+++ src/server/server.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 const loadModel = require('../services/loadModel');
-const InputError =  require('../exceptions/InputError')
  
 (async () => {
     const server = Hapi.server({
@@ -23,15 +22,6 @@ const InputError =  require('../exceptions/InputError')
  
     server.ext('onPreResponse', function (request, h) {
         const response = request.response;
- 
-        if (response instanceof InputError) {
-            const newResponse = h.response({
-                status: 'fail',
-                message: 'Terjadi kesalahan dalam melakukan prediksi'
-            })
-            newResponse.code(response.statusCode)
-            return newResponse;
-        }
 
         if (response.isBoom) {
             if (response.output.statusCode === 413) {
