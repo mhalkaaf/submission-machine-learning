6893eef425a5bb4ae2ad48d2c8d763b5bb1f4252
haeckal <haeckal@asaren.ai>
2024-05-23 08:26:19 +0700

    tes

--- src/server/server.js
+++ src/server/server.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 const loadModel = require('../services/loadModel');
-const InputError = require('../exceptions/InputError');
-const PredictionError = require('../exceptions/PredictionError');
+// const InputError = require('../exceptions/InputError');
+// const PredictionError = require('../exceptions/PredictionError');
  
 (async () => {
     const server = Hapi.server({
@@ -25,23 +25,23 @@ const PredictionError = require('../exceptions/PredictionError');
     server.ext('onPreResponse', function (request, h) {
         const response = request.response;
 
-        if (response instanceof InputError) {
-            const newResponse = h.response({
-                status: 'fail',
-                message: `${response.message} Silakan gunakan foto lain.`
-            });
-            newResponse.code(response.statusCode);
-            return newResponse;
-        }
+        // if (response instanceof InputError) {
+        //     const newResponse = h.response({
+        //         status: 'fail',
+        //         message: `${response.message} Silakan gunakan foto lain.`
+        //     });
+        //     newResponse.code(response.statusCode);
+        //     return newResponse;
+        // }
 
-        if (response instanceof PredictionError) {
-            const newResponse = h.response({
-                status: 'fail',
-                message: 'Terjadi kesalahan dalam melakukan prediksi'
-            });
-            newResponse.code(response.statusCode);
-            return newResponse;
-        }
+        // if (response instanceof PredictionError) {
+        //     const newResponse = h.response({
+        //         status: 'fail',
+        //         message: 'Terjadi kesalahan dalam melakukan prediksi'
+        //     });
+        //     newResponse.code(response.statusCode);
+        //     return newResponse;
+        // }
 
         if (response.isBoom) {
             if (response.output.statusCode === 413) {
